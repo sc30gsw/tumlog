1f9bd1bbd2bc60853d48e706b07e3cd8630f0b19
sc30gsw <haidoushi875@gmail.com>
2020-12-19 12:05:52 +0900

    ユーザー情報を記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname {Faker::Name.name}
+    email {Faker::Internet.free_email}
+    profile { "30歳のサラリーマンです" }
+    password { '1a' + Faker::Internet.Password(min_length: 6)}
+    password_confirmation { password }
   end
 end
