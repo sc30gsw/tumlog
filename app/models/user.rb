d886877c2e955b25893b1d14bb62042d2a6a29df
sc30gsw <haidoushi875@gmail.com>
2020-12-19 11:45:41 +0900

    バリデーションに正規表現を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :profile
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: 'is invalid. Input half-width characters' }
   end
 end
