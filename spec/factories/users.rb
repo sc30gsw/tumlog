bf8c1c993bc453d3e16781ae2c49d398e56ad45f
sc30gsw <haidoushi875@gmail.com>
2020-12-19 12:37:14 +0900

    ユーザー新規登録のテスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     nickname {Faker::Name.name}
     email {Faker::Internet.free_email}
     profile { "30歳のサラリーマンです" }
-    password { '1a' + Faker::Internet.Password(min_length: 6)}
+    password { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
   end
 end
