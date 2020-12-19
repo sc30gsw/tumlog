14c8562c2b29f632327bd6cf25f2df365230bd29
sc30gsw <haidoushi875@gmail.com>
2020-12-19 15:19:12 +0900

    ActiveHashモデルのテーブル作成

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,13 @@
-class Category < ApplicationRecord
+class Category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: "--" }
+    { id: 2, name: "運動" }
+    { id: 3, name: "ダイエット(体重管理)" }
+    { id: 4, name: "勉強" }
+    { id: 5, name: "節約" }
+    { id: 6, name: "マインドフルネス瞑想" }
+  ]
 
-  has_one :room
+  include AcitiveHash::Associations 
+  has_many :rooms
 end
