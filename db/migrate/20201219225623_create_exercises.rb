efa058f54ee8909f68d275df498d994bd4343edd
sc30gsw <haidoushi875@gmail.com>
2020-12-20 19:38:30 +0900

    テーブルの修正

--- db/migrate/20201219225623_create_exercises.rb
+++ db/migrate/20201219225623_create_exercises.rb
@@ -1,6 +1,8 @@
 class CreateExercises < ActiveRecord::Migration[6.0]
   def change
     create_table :exercises do |t|
+      t.text :text, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,49 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_19_231034) do
+ActiveRecord::Schema.define(version: 2020_12_19_225654) do
 
   create_table "daiets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_daiets_on_user_id"
   end
 
   create_table "exercises", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "habits", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text", null: false
     t.bigint "user_id", null: false
-    t.bigint "exercise_id"
-    t.bigint "daiet_id"
-    t.bigint "learn_id"
-    t.bigint "saving_id"
-    t.bigint "mind_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["daiet_id"], name: "index_habits_on_daiet_id"
-    t.index ["exercise_id"], name: "index_habits_on_exercise_id"
-    t.index ["learn_id"], name: "index_habits_on_learn_id"
-    t.index ["mind_id"], name: "index_habits_on_mind_id"
-    t.index ["saving_id"], name: "index_habits_on_saving_id"
-    t.index ["user_id"], name: "index_habits_on_user_id"
+    t.index ["user_id"], name: "index_exercises_on_user_id"
   end
 
   create_table "learns", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_learns_on_user_id"
   end
 
   create_table "minds", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_minds_on_user_id"
   end
 
   create_table "savings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_savings_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -69,10 +66,9 @@ ActiveRecord::Schema.define(version: 2020_12_19_231034) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "habits", "daiets"
-  add_foreign_key "habits", "exercises"
-  add_foreign_key "habits", "learns"
-  add_foreign_key "habits", "minds"
-  add_foreign_key "habits", "savings"
-  add_foreign_key "habits", "users"
+  add_foreign_key "daiets", "users"
+  add_foreign_key "exercises", "users"
+  add_foreign_key "learns", "users"
+  add_foreign_key "minds", "users"
+  add_foreign_key "savings", "users"
 end
