76c3f37f7672c384c303faa364611fc7a9a25c57
sc30gsw <haidoushi875@gmail.com>
2020-12-20 17:41:36 +0900

    バリデーションの設定

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -6,4 +6,9 @@ class Habit < ApplicationRecord
   belongs_to :learn
   belongs_to :saving
   belongs_to :mind
+
+  with_options presence: true do
+    validates :text
+    validates :user
+  end
 end
