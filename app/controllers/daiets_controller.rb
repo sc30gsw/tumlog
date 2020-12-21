ba9b95939ea3d847a576247d63d7a36d998f2593
sc30gsw <haidoushi875@gmail.com>
2020-12-21 14:40:47 +0900

    daietsコントローラーの設定

--- app/controllers/daiets_controller.rb
+++ app/controllers/daiets_controller.rb
@@ -1,4 +1,27 @@
 class DaietsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
+    @daiets = Daiet.all
+  end
+
+  def new
+    @daiet = Daiet.new
+  end
+
+  def create
+    @daiet = Daiet.new(daiet_params)
+    if @daiet.valid?
+      @daiet.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def daiet_params
+    params.require(:daiet).permit(:text, :image).merge(user_id: current_user.id)
   end
 end
