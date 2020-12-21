b588745b1d11c5420904604e9407e4c915b72c4c
sc30gsw <haidoushi875@gmail.com>
2020-12-21 17:13:16 +0900

    exerciseモデルの単体テスト

--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -7,6 +7,26 @@ RSpec.describe Exercise, type: :model do
 
   describe '新規投稿' do
     context '新規投稿がうまくいくとき' do
-      it 'textのみあれば保存できる'
+      it 'textが空でも保存できる' do
+        @exercise.text = nil
+        expect(@exercise).to be_valid
+      end
+      it '画像が空でも保存できる' do
+        @exercise.image = nil
+        expect(@exercise).to be_valid
+      end
+      it 'textと画像の両方があれば保存できる' do
+        expect(@exercise).to be_valid
+      end
+    end
+
+    context '新規投稿がうまくいかないとき' do
+      it 'textと画像の療法が空だと保存できない' do
+        @exercise.text = nil
+        @exercise.image = nil
+        @exercise.valid?
+        expect(@exercise.errors.full_messages).to include("Text can't be blank")
+      end
     end
   end
+end
