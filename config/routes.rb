5f1f3fd45a77cc31d7e3fae5666f3b4800ba1c4b
sc30gsw <haidoushi875@gmail.com>
2020-12-21 11:36:26 +0900

    記述の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'tops#index'
-  resources :exercises, only: :index
+  resources :exercises, only: [:index, :new, :create] 
   resources :daiets, only: :index
   resources :learns, only: :index
   resources :savings, only: :index
   resources :minds, only: :index
+
 end
