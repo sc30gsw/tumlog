fd7540ee97cf738f025fbe70dc8ca8c75fc84ffb
sc30gsw <haidoushi875@gmail.com>
2020-12-21 11:56:21 +0900

    exercisesコントローラーのアクションの定義

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,4 +1,24 @@
 class ExercisesController < ApplicationController
   def index
+    @exercises = Exercise.all
+  end
+
+  def new
+    @exercise = Exercise.new
+  end
+
+  def create
+    @exercise = Exercise.new(exercise_params)
+    if @exercise.valid?
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def exercise_params
+    params.require(:exercise).permit(:text, :image).merge(user_id: current_user.id)
   end
 end
