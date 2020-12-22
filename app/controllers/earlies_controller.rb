c8e7f5656c4bfdd95886f7b50a486f3a4abebc41
sc30gsw <haidoushi875@gmail.com>
2020-12-22 10:28:45 +0900

    earlyコントローラーのアクション定義

--- app/controllers/earlies_controller.rb
+++ app/controllers/earlies_controller.rb
@@ -1,10 +1,31 @@
 class EarliesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    @earlies = Early.all
   end
 
   def new
+    @early = Early.new
+  end
+  
+  def create
+    @early = Early.new(early_params)
+    if @early.vali?
+      @early.save
+      redirect_to action: :index
+    else
+      render :new
+    end
   end
 
   def show
+    @early = Early.find(params[:id])
+  end
+
+  private
+
+  def early_params
+    params.require(:early).permit(:text, :image).merge(user_id: current_user.id)
   end
 end
