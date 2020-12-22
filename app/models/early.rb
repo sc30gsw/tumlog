0225c4c6890fa28765b683d3e684ed6db2e727fb
sc30gsw <haidoushi875@gmail.com>
2020-12-22 10:15:35 +0900

    earlyモデルの設定

---
+++ app/models/early.rb
@@ -0,0 +1,11 @@
+class Early < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  validates :text, presence: true, unless: :was_attached?
+  validates :user, presence: true
+
+  def was_attached?
+    self.image.attached?
+  end
+end
