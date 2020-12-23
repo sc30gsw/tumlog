f8810386d35afc3ccbd46235fef103e9edf19ca9
sc30gsw <haidoushi875@gmail.com>
2020-12-23 09:52:31 +0900

    ユーザーに紐づく投稿をすべて取得

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @exercises = @user.exercises.all.order(id: 'DESC')
+    @daiets = @user.daiets.all.order(id: 'DESC')
+    @learns = @user.learns.all.order(id: 'DESC')
+    @savings = @user.savings.all.order(id: 'DESC')
+    @minds  = @user.minds.all.order(id: 'DESC')
+    @earlies = @user.earlies.all.order(id: 'DESC')
   end
 
 end
