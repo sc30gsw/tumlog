35c75fa05d50a0a5f9027615c46636394dad7fb0
sc30gsw <haidoushi875@gmail.com>
2020-12-23 12:10:05 +0900

    マイページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     @daiets = @user.daiets.all.order(id: 'DESC')
     @learns = @user.learns.all.order(id: 'DESC')
     @savings = @user.savings.all.order(id: 'DESC')
-    @minds  = @user.minds.all.order(id: 'DESC')
+    @minds = @user.minds.all.order(id: 'DESC')
     @earlies = @user.earlies.all.order(id: 'DESC')
   end
 
