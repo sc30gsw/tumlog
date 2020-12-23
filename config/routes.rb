132ccb307d3194ea62a645a7613f2de30e3e9f21
sc30gsw <haidoushi875@gmail.com>
2020-12-23 18:42:05 +0900

    検索のルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,26 +3,44 @@ Rails.application.routes.draw do
   root to: 'tops#index'
   resources :exercises do
     resources :exercise_comments, only: :create
+    collection do
+      get 'search'
+    end
   end
 
   resources :daiets do
     resources :daiet_comments, only: :create
+    collection do
+      get 'search'
+    end
   end
 
   resources :learns do
     resources :learn_comments, only: :create
+    collection do
+      get 'search'
+    end
   end
 
   resources :savings do
     resources :saving_comments, only: :create
+    collection do
+      get  'search'
+    end
   end
 
   resources :minds do
     resources :mind_comments, only: :create
+    collection do
+      get 'search'
+    end
   end
 
   resources :earlies do
     resources :early_comments, only: :create
+    collection do
+      get 'search'
+    end
   end
   
   resources :users, only: :show
