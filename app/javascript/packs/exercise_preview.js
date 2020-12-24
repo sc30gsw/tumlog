9662303a26bc0913ec4ea1dbd2566c5c15d82359
sc30gsw <haidoushi875@gmail.com>
2020-12-25 08:44:10 +0900

    取得した画像のURL情報を取得

--- app/javascript/packs/exercise_preview.js
+++ app/javascript/packs/exercise_preview.js
@@ -1,7 +1,11 @@
 function preview(){
   const imageList = document.getElementById('image-list');
   document.getElementById('exercise_image').addEventListener('change', function (e){
-    console.log(e);
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+   /*  const imageElement = document.createElement('div');
+    const blobImage = document.createElement('img'); */
   })
 }
 window.addEventListener('load', preview);
