b61825c2c3fc6b1b2ea75bf3b9fc0b8a325cac39
sc30gsw <haidoushi875@gmail.com>
2020-12-25 08:35:38 +0900

    イベントでファイルの内容を取得

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,7 +1,7 @@
 function preview(){
   const imageList = document.getElementById('image-list');
-  document.getElementById('exercise_image').addEventListener('change', (e) => {
-    console.log(123);
-  });
+  document.getElementById('exercise_image').addEventListener('change', function (e){
+    console.log(e);
+  })
 }
 window.addEventListener('load', preview);
