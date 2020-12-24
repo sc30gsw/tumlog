d1f796efb82f342cb75d3b39eee5e77f51dcf534
sc30gsw <haidoushi875@gmail.com>
2020-12-25 08:48:52 +0900

    プレビュー画像の表示

--- app/javascript/packs/exercise_preview.js
+++ app/javascript/packs/exercise_preview.js
@@ -6,6 +6,10 @@ function preview(){
 
     const imageElement = document.createElement('div');
     const blobImage = document.createElement('img');
+    blobImage.setAttribute('src', blob);
+
+    imageElement.appendChild(blobImage);
+    imageList.appendChild(imageElement);
   })
 }
 window.addEventListener('load', preview);
