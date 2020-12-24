5f341a718a51c8c963cf184cca85df95881fff3a
sc30gsw <haidoushi875@gmail.com>
2020-12-25 08:29:18 +0900

    ファイル選択をするとイベント発火するよう設定

---
+++ app/javascript/packs/preview.js
@@ -0,0 +1,7 @@
+function preview(){
+  const imageList = document.getElementById('image-list');
+  document.getElementById('exercise_image').addEventListener('change', (e) => {
+    console.log(123);
+  });
+}
+window.addEventListener('load', preview);
