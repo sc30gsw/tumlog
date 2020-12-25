4a733ce6a4c80188d475f23afa1d0ada8b794e5a
sc30gsw <haidoushi875@gmail.com>
2020-12-25 10:08:39 +0900

    既存の画像の削除

--- app/javascript/packs/exercise_preview.js
+++ app/javascript/packs/exercise_preview.js
@@ -3,11 +3,11 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
     const ImageList = document.getElementById('image-list');
 
     document.getElementById('exercise_image').addEventListener('change', function(e){
-/* 
+
       const imageContent = document.querySelector('img');
       if (imageContent){
         imageContent.remove();
-      } */
+      }
 
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
