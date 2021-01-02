b180c8230f25dbf561ed704cf0e67555b2e8b6c9
sc30gsw <haidoushi875@gmail.com>
2021-01-02 17:45:37 +0900

    binding.pryの削除

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -11,7 +11,6 @@ class ExercisesController < ApplicationController
   end
 
   def create
-    binding.pry
     @exercise = Exercise.new(exercise_params)
     if @exercise.valid?
       @exercise.save
